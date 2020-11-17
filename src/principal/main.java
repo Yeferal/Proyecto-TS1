e264904b92ae1e67d0d82153ce0741dd84843e59
LouisGonzalez <louiscrack50@gmail.com>
2020-11-17 01:10:19 -0600

    actualizacion

--- src/principal/main.java
+++ src/principal/main.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.UnsupportedLookAndFeelException;
 import principal.frontend.gui.calendari_cholquij.FrameCalendario;
+import principal.menu.MenuPrincipal;
 
 /**
  *
@@ -20,14 +21,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        try {
-            // TODO code application logic here
-            javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-            JOptionPane.showMessageDialog(null, "No se encuentra el estilo, por lo tanto se utlizara por defecto...");
-        }        
-        FrameCalendario calendarioCholquij = new FrameCalendario();
-        calendarioCholquij.setVisible(true);
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
     }
 }
