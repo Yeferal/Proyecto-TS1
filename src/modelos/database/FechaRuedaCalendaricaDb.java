b323eabdf5b6f311133680e768cce579908df7e1
Carlos Soberanis <jcsru13@gmail.com>
2020-11-17 03:38:07 -0600

    Update: metodos CRUD (cargador, calendarioHaab, rueda_calendarica, nahual, informacion)

--- src/modelos/database/FechaRuedaCalendaricaDb.java
+++ src/modelos/database/FechaRuedaCalendaricaDb.java
@@ -5,10 +5,50 @@
  */
 package modelos.database;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelos.objetos.FechaRuedaCalendarica;
+
 /**
  *
  * @author jose_
  */
 public class FechaRuedaCalendaricaDb {
     
+    public void crear(FechaRuedaCalendarica fecha){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("INSERT INTO RuedaCalendarica"
+                    + "(idFechaHaab,idFechaCholqij,Descripcion) VALUES (?,?,?);");
+            statement.setInt(1, fecha.getFechaHaab().getId());
+            statement.setInt(2, fecha.getFechaCholqij().getId());
+            statement.setString(3, fecha.getDescripcion());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void modificar(FechaRuedaCalendarica fecha){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("UPDATE RuedaCalendarica SET"
+                    + "idFechaHaab=?, idFechaCholqij=?, descripcion=? WHERE id=?;");
+            statement.setInt(1, fecha.getFechaHaab().getId());
+            statement.setInt(2, fecha.getFechaCholqij().getId());
+            statement.setString(3, fecha.getDescripcion());
+            statement.setInt(4, fecha.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void eliminar(FechaRuedaCalendarica fecha){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("DELETE FROM RuedaCalendarica WHERE id=?;");
+            statement.setInt(1, fecha.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- src/modelos/database/InformacionDb.java
+++ src/modelos/database/InformacionDb.java
@@ -5,10 +5,48 @@
  */
 package modelos.database;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelos.objetos.Informacion;
+
 /**
  *
  * @author jose_
  */
 public class InformacionDb {
     
+    public void crear(Informacion informacion){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("INSERT INTO Informacion "
+                    + "(titulo,descripcion) VALUES (?,?);");
+            statement.setString(1, informacion.getTitulo());
+            statement.setString(2, informacion.getDescripcion());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void modificar(Informacion informacion){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("UPDATE Informacion SET "
+                    + "titulo=?, descripcion=? WHERE id=?;");
+            statement.setString(1, informacion.getTitulo());
+            statement.setString(2, informacion.getDescripcion());
+            statement.setInt(3, informacion.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void eliminar(Informacion informacion){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("DELTE FROM Informacion WHERE id=?;");
+            statement.setInt(1, informacion.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
