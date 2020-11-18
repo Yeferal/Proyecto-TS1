5438afdeb4aafdb7ccdb1692a756111eefaee6a0
Esmeralda Vargas <32379907+CELIAVARGAS@users.noreply.github.com>
2020-11-18 07:15:42 -0600

    creacion de clase

---
+++ src/modelos/database/CategorizarDB.java
@@ -0,0 +1,11 @@
+package modelos.database;
+
+/**
+ *
+ * @author esmeralda
+ */
+public class CategorizarDB {
+    //clasifica el hecho historico y la categoria
+    //solo muestra como deberia de estar los datos enlistados 
+    
+}
