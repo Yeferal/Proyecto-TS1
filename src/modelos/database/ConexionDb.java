fd0ba509090e54630b1327418a268a9b3de71b66
Esmeralda Vargas <32379907+CELIAVARGAS@users.noreply.github.com>
2020-11-18 07:08:44 -0600

    conexion DB

--- src/modelos/database/ConexionDb.java
+++ src/modelos/database/ConexionDb.java
@@ -6,6 +6,8 @@
 package modelos.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -13,5 +15,36 @@ import java.sql.Connection;
  */
 public class ConexionDb {//Aquí la conexion a la base de datos
     
-    public static Connection conexion;
+    public static Connection conexion = null;
+
+    //llenar con credenciales de su DB , datos de ejemplo
+    final static String NombreBaseDatos = "CalendarioMaya";
+    final static String usuario = "root";
+    final static String password = "1234";
+
+    //Nos conectamos a nuestra db
+    public static  Connection obtenerConexion() throws SQLException, ClassNotFoundException {
+        if (conexion == null) {
+            try {
+                //Buscamos nuestra db por medio de su ubicacion <entorno  local>, mandando el nombre y nuestra password
+                Class.forName("com.mysql.jdbc.Driver");
+                conexion = DriverManager.getConnection("jdbc:mysql://localhost/" + NombreBaseDatos, usuario, password);
+            }//en caso de que no se encuentre la base de datos 
+            catch (SQLException ex) {
+                throw new SQLException(ex);
+            }//en caso de que no encuentre la libreria 
+            catch (ClassNotFoundException ex) {
+                throw new ClassCastException(ex.getMessage());
+            }
+        }
+        return conexion;
+    }
+
+    //cerramos conexion , liberamos nuestra DB 
+    public static void cerrar() throws SQLException {
+        if (conexion != null) {
+            conexion.close();
+        }
+    }
+
 }
