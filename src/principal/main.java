db0b280f5898d1010d1c525340bf4520070d302e
LouisGonzalez <louiscrack50@gmail.com>
2020-11-18 12:16:19 -0600

    agregando try catch a la conexion

--- src/principal/main.java
+++ src/principal/main.java
@@ -5,6 +5,11 @@
  */
 package principal;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelos.database.ConexionDb;
+
 /**
  *
  * @author luisGonzalez
@@ -15,14 +20,20 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-                //INICIAMOS CONECTANDO  DB
-        ConexionDb.obtenerConexion();
-
-        
-                //CERRAMOS CONEXION DB
-        ConexionDb.cerrar();
+        try {
+            // TODO code application logic here
+            
+            //INICIAMOS CONECTANDO  DB
+            ConexionDb.obtenerConexion();
+            
+            
+            //CERRAMOS CONEXION DB
+            ConexionDb.cerrar();
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 }
