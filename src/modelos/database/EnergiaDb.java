67bb1f93e552d04868005fab037a53088a4e3bbd
Carlos Soberanis <jcsru13@gmail.com>
2020-11-17 19:18:27 -0600

    Update: CRUD y manipulacion de entidad Energia

--- src/modelos/database/EnergiaDb.java
+++ src/modelos/database/EnergiaDb.java
@@ -5,10 +5,80 @@
  */
 package modelos.database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import modelos.objetos.Energia;
+
 /**
  *
  * @author jose_
  */
 public class EnergiaDb {
+
+    public void crear(Energia energia){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("INSERT INTO Energia "
+                    + "(nombre) VALUES (?);");
+            statement.setString(1, energia.getNombre());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
     
+    public void modificar(Energia energia){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("UPDATE Energia SET "
+                    + "nombre=? WHERE id=?;");
+            statement.setString(1, energia.getNombre());
+            statement.setInt(2, energia.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void eliminar (Energia energia){
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("DELETE FROM Energia WHERE id=?;");
+            statement.setInt(1, energia.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public List<Energia> getEnergias(){
+        List<Energia> energias = new ArrayList();
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("SELECT * FROM Energia;");
+            ResultSet resultado = statement.executeQuery();
+            while(resultado.next()) energias.add(instanciarDeResultSet(resultado));
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return energias;
+    }
+    
+    public Energia getEnergia(int id) {
+        try {
+            PreparedStatement statement = ConexionDb.conexion.prepareStatement("SELECT * FROM Energia WHERE id=?;");
+            statement.setInt(1, id);
+            ResultSet resultado = statement.executeQuery();
+            if(resultado.next()) return instanciarDeResultSet(resultado);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    private Energia instanciarDeResultSet(ResultSet resultado) throws SQLException{
+        return new Energia(
+                resultado.getInt("id"),
+                resultado.getString("nombre")
+        );
+    }
 }
