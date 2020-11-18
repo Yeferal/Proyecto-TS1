5990fabdf417d72a6a2dae17b194a2f80d5a3440
Esmeralda Vargas <32379907+CELIAVARGAS@users.noreply.github.com>
2020-11-18 07:11:46 -0600

    toString en Rol

--- src/modelos/objetos/Rol.java
+++ src/modelos/objetos/Rol.java
@@ -37,5 +37,10 @@ public class Rol {
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
+        @Override
+    public String toString() {
+        return "\nRol{" + "id=" + id + ", tipo=" + tipo + '}';
+    }
+
     
 }
